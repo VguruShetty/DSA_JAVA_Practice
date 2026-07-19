ccafb9d9542ab84e452a060c0ded64c6851d6782
vguru shetty <vgurushetty@gmail.com>
2026-07-19 23:51:32 +0530

    Counting sort

---
+++ CountingSort.java
@@ -0,0 +1,8 @@
+package DSA_JAVA_Practice;
+
+public class CountingSort {
+    public static void main(String[] args){
+        int[] arr = {2, 3, 0, 2, 3, 2};
+        int n = arr.length;
+    }
+}
