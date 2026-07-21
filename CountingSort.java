352db30703ca421a40d1c85d6e50ece4f78850e5
vguru shetty <vgurushetty@gmail.com>
2026-07-21 23:49:40 +0530

    CountingSort

--- CountingSort.java
+++ CountingSort.java
@@ -1,8 +1,39 @@
 package DSA_JAVA_Practice;
 
 public class CountingSort {
+    public static void countingSortArray(int[] arr){
+        if (arr == null || arr.length == 0) return;
+        
+        int maxVal = arr[0];
+        for(int val : arr){
+            if(val > maxVal){ //  Fixed: Changed '<' to '>' to find the maximum
+                maxVal = val;
+            }
+        }
+        
+        int[] count = new int[maxVal+1];
+        for(int num : arr){
+            count[num]++;
+        }
+        
+        int index = 0;
+        for(int i = 0; i <= maxVal; i++ ){
+            while(count[i] > 0){
+                arr[index] = i;
+                index++;
+                count[i]--;
+            }
+        }
+    }
+    
     public static void main(String[] args){
         int[] arr = {2, 3, 0, 2, 3, 2};
-        int n = arr.length;
+        countingSortArray(arr);
+
+        System.out.print("Sorted Array : ");
+        for(int value : arr){
+            System.out.print(value + " "); // Added space for clean formatting
+        }
+        System.out.println();
     }
 }
