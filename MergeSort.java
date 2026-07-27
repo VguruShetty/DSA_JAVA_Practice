fc306841d0d01b52a34a285b3ed61d19bf978f02
vguru shetty <vgurushetty@gmail.com>
2026-07-27 23:07:34 +0530

    Updated Merge Sort

--- MergeSort.java
+++ MergeSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class MergeSort{
     public static void main(String[] args){
-        int[] unsortedArr = {3, 7, 6, -10, 1, 23, 5, 55, -13};
+        int[] unsortedArr = {5, 10, 90, 50, 80, 2, 1, 3};
         int[] sortedArr = MergeSorting(unsortedArr);
         System.out.println("Sorted Array : " + Arrays.toString(sortedArr));
     }
@@ -15,13 +15,31 @@ public class MergeSort{
         }
         int mid = arr.length / 2;
         int[] LeftArr = Arrays.copyOfRange(arr, 0, mid);
+        System.out.print("Left array : ");
+        for(int l : LeftArr){
+            System.out.print(l + " ");
+        }
         int[] rightArr = Arrays.copyOfRange(arr, mid, arr.length);
+         System.out.print("Left array : ");
+        for(int l : rightArr){
+            System.out.print(l + " ");
+        }
         int[] sortedLeft = MergeSorting(LeftArr);
         int[] sortedRight = MergeSorting(rightArr);
         return merge(sortedLeft, sortedRight);
      }
      public static int[] merge(int[] leftArr, int[] rightArr){
         int[] result = new int[leftArr.length + rightArr.length];
+        System.out.print("Left array : ");
+        for(int l : leftArr){
+            System.out.print(l + " ");
+        }
+        System.out.println();
+        System.out.print("Right array : ");
+        for(int r : leftArr){
+            System.out.print(r + " ");
+        }
+        System.out.println();
         int i=0, j=0, k=0;
         while(i < leftArr.length && j < rightArr.length){
             if(leftArr[i] < rightArr[j]){
